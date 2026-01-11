c4229c10c42368516f2ff89cc25f3f5f61b02279
cheva360 <sebastianmagana360@gmail.com>
2026-01-11 01:50:27 -0800

    1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,7 @@ public class Player : MonoBehaviour
     [SerializeField] private int _numSeeds = 5; 
     [SerializeField] private PlantCountUI _plantCountUI;
 
-    private int _numSeedsLeft;
+    private int _numSeedsLeft = 5;
     private int _numSeedsPlanted;
 
     private void Start ()
@@ -18,11 +18,27 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        
+        //wasd movement
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(horizontal, vertical, 0f);
+        _playerTransform.Translate(direction.normalized * _speed * Time.deltaTime, Space.World);
+
+        //on space key pressed plant seed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlantSeed();
+        }
     }
 
     public void PlantSeed ()
     {
-        
+        if (_numSeedsLeft > 0)
+        {
+            Instantiate(_plantPrefab, _playerTransform.position, Quaternion.identity);
+            _numSeedsLeft--;
+            _numSeedsPlanted++;
+            _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
+        }
     }
 }
